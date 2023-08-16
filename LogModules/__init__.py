18178d18cbbbc9864cd3b4dde3b6c0639f41fd0b
neades2305 <nathan.eades@permiso.io>
2023-08-15 21:57:41 -0400

    Opensource Updates: simplification, cleanup, ease of use

--- LogModules/__init__.py
+++ LogModules/__init__.py
@@ -1,5 +1,4 @@
 from . import data_load
 from . import data_search
-from .output import write
-
-##bloop
+from .output import write_manifest
+from .output import get_manifest_filename
--- RunLogLicker.py
+++ RunLogLicker.py
@@ -1,46 +1,52 @@
-import LogModules
+from LogModules import data_load, data_search, write_manifest, get_manifest_filename
+
+asciiImage: str = '''
+  88      dP.Yb   dPEEb8 88     88  dPTTb8 77  dP 867888 88<>Yb
+  AA     EE   Yb dP      88     88 dP      88edP  88__   88__dP
+  88  DD Yb   dP Yb  X88 88     88 Yb      99mYb  88""   88VYb
+  88ooBB  Yb.dP   YboodP 88ood8 88  YbZZdP 88  Yb 882283 88  Yb
+'''
 
 if __name__ == "__main__":
-    #getting args from parser
-    args: dict = LogModules.data_load.load_args()
-    
-    #formatting args for kwarg function call
+    print(asciiImage)
+
+    args: dict = data_load.load_args()
+
     loadFilesArgs: dict = {
       "exrexFile": args.get("exrexFilePath"),
       "regexFile": args.get("regexFilePath"),
       "manifestFile": args.get("inputManifestFilePath"),
       "stringList": args.get("stringList")
     }
-    
-    #grabbing inputed dictionaries from files
-    exrexDict: dict | None
-    regexDict: dict | None
-    manifest: dict | None
-    exrexDict, regexDict, manifest = LogModules.data_load.load_files(**loadFilesArgs)
+
+    #load and verify manifest/regexes
+    exrexDict, regexDict, manifest = data_load.load_files(**loadFilesArgs)
     deAnonymize: bool = args.get("deAnonymize")
     regexList: list = args.get("regexList")
-    
-    #writing whatever's requested to file
-    manifest = LogModules.data_search.modify_manager(
-      args["inputCase"], 
-      args["inputData"], 
+
+    #write details to file
+    manifest, dataHash = data_search.modify_manager(
+      args.get("inputCase"),
+      args.get("inputData"),
       regexDict,
-      exrexDict, 
-      manifest, 
-      args["outputFilePath"],
+      exrexDict,
+      manifest,
+      args.get("outputFilePath"),
       deAnonymize,
       regexList
     )
-    
-    #setting up args    
+
+    #rewrite manifest file name with hash
+    if dataHash and args.get("outputManifestFilePath"):
+        updateManifestFilePath = get_manifest_filename(args.get("outputManifestFilePath"), dataHash)
+        if updateManifestFilePath:
+            args['outputManifestFilePath'] = updateManifestFilePath
+
     writeArgs: dict = {
       "manifest": manifest,
       "manifestFilePath": args.get("outputManifestFilePath")
     }
-    
-    
-    if "outputManifestFilePath" in args:
-        writeArgs["manifestFilePath"] = args["outputManifestFilePath"]
-    
-    LogModules.write(**writeArgs)
-    
+
+    write_manifest(**writeArgs)
+
+    print(f'[*] Completed. - {dataHash}')
